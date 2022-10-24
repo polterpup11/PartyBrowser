90c1693ae944b301e7036ca9680b1617fa7fa0c1
polterpup11 <89555773+polterpup11@users.noreply.github.com>
2022-10-23 23:55:28 -0400

    The Code to this.

---
+++ PartyWeb.vb
@@ -0,0 +1,14 @@
+﻿Public Class Form1
+
+    Private Sub Button1_Click(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles Button1.Click
+        WebBrowser1.Navigate(TextBox1.Text)
+    End Sub
+
+    Private Sub Form1_Load(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles MyBase.Load
+
+    End Sub
+
+    Private Sub Button2_Click(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles Button2.Click
+        WebBrowser1.GoBack()
+    End Sub
+End Class
